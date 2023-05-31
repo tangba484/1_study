a1d64f24605e756ead45210a3697eedc494a19f2
sangyoung <tangba484@naver.com>
2023-06-01 00:35:30 +0900

    first

--- baekjoon_1629.py
+++ baekjoon_1629.py
@@ -1,6 +1,12 @@
-tree={}
+a,b,c = map(int,input().split())
 
-while 1:
-    try:
-        node = int(input())
-        
+def R(a,b,c):
+    if b == 1 :
+        return a%c
+    
+    ans = R(a,b//2,c)
+    if b%2 == 1:
+        return ans*ans*a%c
+    else:
+        return ans*ans%c
+print(R(a,b,c))
