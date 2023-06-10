61dceb5bd9c8c0fe812745007bff9be85d4ee7b7
sangyoung <tangba484@naver.com>
2023-06-10 20:38:53 +0900

    first

--- pra.py
+++ pra.py
@@ -1,12 +1,3 @@
-a,b,c = map(int,input().split())
+a,b = map(int,input().split())
 
-k = 1
-
-while k <= c:
-    if a >= 2*b:
-        a=a-1
-        k+=1
-    else:
-        b = b-1
-        k+=1
-print(min(int(a/2),b))
+print(a+b)
