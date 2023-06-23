a717ac19aea9aab9f8492cc464485a81400bccf1
sangyoung <tangba484@naver.com>
2023-06-23 13:09:25 +0900

    first

--- pra.py
+++ pra.py
@@ -1,7 +1,2 @@
-n = int(input())
-x,y = [],[]
-for _ in range(n):
-    a,b = map(int,input().split())
-    x.append(a)
-    y.append(b)
-print((max(x)-min(x))*(max(y)-min(y)))
+n = 66+78.8/5
+print(n)
