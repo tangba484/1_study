b32e9c07c6c92937bdb259c2dd0b485cd1613081
sangyoung <tangba484@naver.com>
2023-06-25 23:29:42 +0900

    first

--- .pra.py
+++ .pra.py
@@ -1,10 +1,19 @@
-def solution(w, h):
-    answer = 0
-    a = max(w,h)
-    b = min(w,h)
-    for i in range(1, min(w,h)):
+num = int(input())
+for _ in range(num):
+    x1, y1, r1, x2, y2, r2 = map(int, input().split(' '))
+    d = ((x1 - x2) ** 2 + (y1 - y2) ** 2) **(1/2)
+    if x1 == x2 and y1 == y2 and r1 == r2:
         
-        n = int(a*i) // b
-        answer += n
-
-    return 2*answer
+        print(-1)
+    elif r1 == 0:
+        print(1)
+    elif d == r1 + r2:
+        print(1)
+    elif d == abs(r1 - r2):
+        print(1)
+    elif d > r1 + r2:
+        print(0)
+    elif min(r1, r2) < max(r1, r2) - d:
+        print(0)
+    else:
+        print(2)
