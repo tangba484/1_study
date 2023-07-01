5f2585b1d98fa94596559e81c1669e22fe2bc223
sangyoung <tangba484@naver.com>
2023-07-02 05:18:03 +0900

    first

--- .pra.py
+++ .pra.py
@@ -1,7 +1,2 @@
-def gcd(a,b):
-    if b == 0:
-        return a
-    else:
-        return gcd(b,a%b)
-a,b=  map(int,input().split())
-print(int((a*b)/gcd(a,b)))
+l = [" ","_","0","a","A"]
+print(sorted(l))
