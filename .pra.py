7dc5bde66a897bca7b57a9ed95b3eb175ab5e638
sangyoung <tangba484@naver.com>
2023-07-09 18:24:20 +0900

    first

--- .pra.py
+++ .pra.py
@@ -1,1 +1,15 @@
-print("12"+"8")
+while 1:
+    x,y,z = map(int,input().split())
+    if (x,y,z) ==(0,0,0):
+        break
+    T = sorted([x,y,z])
+    
+    if T[2] >= T[1] + T[0]:
+        print("Invalid")
+        continue
+    if T[0] == T[1] and T[1]==T[2]:
+        print("Equilateral")
+    elif T[0]==T[1] or T[1]==T[2]:
+        print("Isosceles")
+    else:
+        print("Scalene")
