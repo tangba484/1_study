9ec9d9fa47b87c2788fc1aaf930b74feae49fe95
sangyoung <tangba484@naver.com>
2023-08-05 20:24:24 +0900

    first

--- .pra.py
+++ .pra.py
@@ -1,21 +1,13 @@
-import sys
-sys.setrecursionlimit(10**5)
+n = int(input())
 
-n,r,c = map(int,input().split())
+k = 0
+if n==1:
+    print(1)
+else:
 
-def R(n,r,c):
-    if n == 1:
-        return 2*r + c
-    l = 2**n
-    half = l/2
-    if r < half and c < half :
-        ans =  R(n-1,r,c)
-    if r<half and c >= half:
-        ans =  half*half+R(n-1,r,c-half)
-    if r>=half and c < half:
-        ans =  half*half*2 + R(n-1,r-half,c)
-    if r>=half and c >= half:
-        ans = 3*half*half + R(n-1,r-half,c-half)
-    return ans
-
-print(int(R(n,r,c)))
+    while 1:
+        k+= 1
+        if 2**k > n:
+            k -= 1
+            break
+    print(k)
